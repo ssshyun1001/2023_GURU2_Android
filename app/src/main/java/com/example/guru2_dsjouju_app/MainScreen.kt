8a4493549810f8bcc82f9886ba9635a3e685b01b
Jasmin527 <yeseojasmin@gmail.com>
2024-07-28 21:23:04 +0900

    로그인 버튼 누르면 메인화면 이동 + 통화 받기/거절 버튼 누를 때 layout/activyty 화면으로 넘어가는 코드 (아직 수정 중)

---
+++ app/src/main/java/com/example/guru2_dsjouju_app/MainScreen.kt
@@ -0,0 +1,14 @@
+package com.example.guru2_dsjouju_app
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.guru2_dsjouju_app.R
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
